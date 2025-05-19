a9d34aea780b4807083b0b9768e9351b38cdb4d9
mohammedQadumy <mohammadqadumy@gmail.com>
2025-05-19 21:18:15 +0300

    started-SignUp-page

--- Frontend/expense-tracker/src/App.jsx
+++ Frontend/expense-tracker/src/App.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+import SignUp from "./pages/Auth/SignUp";
+import Login from "./pages/Auth/Login";
+import Home from "./pages/Dashboard/Home";
+import Income from "./pages/Dashboard/Income";
+import Expense from "./pages/Dashboard/Expense";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
-const App = ()=>{
+const App = () => {
   return (
-    <div className='text-3xl text-red-400'>App</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />}></Route>
+          <Route path="/login" exact element={<Login />}></Route>
+          <Route path="/signUp" exact element={<SignUp />}></Route>
+          <Route path="/dashboard" exact element={<Home />}></Route>
+          <Route path="/income" exact element={<Income />}></Route>
+          <Route path="/expense" exact element={<Expense />}></Route>
+        </Routes>
+      </Router>
+    </div>
   );
+};
 
+export default App;
 
-}
+const Root = () => {
+  const isAuthenticated = !!localStorage.getItem("token");
 
-export default App
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
